bdd974c0c8868f589908e02220dd63091382a063
Thiago Corrêa da Fonseca <thiagocorreafonseca@gmail.com>
2020-09-26 14:46:53 -0300

    Adicionando ViewComponent

---
+++ MinhaDemoMVC/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace MinhaDemoMVC.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
